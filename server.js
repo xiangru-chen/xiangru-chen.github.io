4aee3cdbcc8a433672c578a0a86e9ae310b32b3d
xiangru-chen <160882968+xiangru-chen@users.noreply.github.com>
2024-08-07 14:25:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-import { createWorker } from 'tesseract.js';
+const express = require('express');
+const path = require('path');
+const app = express();
 
-(async () => {
-  const worker = await createWorker('eng');
-  const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-  console.log(ret.data.text);
-  await worker.terminate();
-})();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
