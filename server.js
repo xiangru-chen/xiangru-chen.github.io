c410ac0e9372528ef882a80f8d10ce9acd2e9cb1
xiangru-chen <160882968+xiangru-chen@users.noreply.github.com>
2024-08-07 14:23:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+import { createWorker } from 'tesseract.js';
 
+(async () => {
+  const worker = await createWorker('eng');
+  const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log(ret.data.text);
+  await worker.terminate();
+})();
